f579e7feb5d980d4bf83f41af66ec363e7dd1232
siddharthtiwari7742-cyber <siddharthtiwari7742@gmail.com>
2026-07-14 21:30:30 +0000

    watched upto half of lecture 11

---
+++ basics/lecture11-14.js
@@ -0,0 +1,14 @@
+const name="Siddh";
+const roll_no=50;
+//console.log(name+roll_no+" value");
+// better version is variable inject karne wala tarreka 
+// python ki formatted string also string methods laga sakte 
+
+console.log('hello my name is ${name} and my rool number is ${roll_no}');
+
+const gamename= new String('Siddharth');
+//isme many methods miljate  aur index miljatti
+// object hogga yeha pe 
+// ye sab rightclick->inspect->console mai jaake milta 
+
+console.log(name.__proto__);
--- basics/lecture7-10.js
+++ basics/lecture7-10.js
@@ -23,3 +23,43 @@ console.log("1"+2);
 console.log(1+"2");
 console.log("1"+2+2);
 console.log(1+2+"2");
+//-------------------------summary of dataypes-----------------------------------------------------------------------
+// primitive->String, number ,boollean , null(this is not same as 0), undefined(value not yet given ),symbol(used to make something unique)
+//these are call by vale that is their  copy is passed instead of reference but for non primitve referenceis passed 
+//non-primitive called by reference-> array, objeccts , function
+const id =Symbol('123');
+const another_id=Symbol('123');
+console.log(id===another_id);
+// even though same string was passed their Symbol is not same 
+const arr=['Panner','Chole','pizza'];//
+const myobj={
+    name:"sidd",
+    age:18,
+}
+const func=function(){
+    console.log("hello world");
+}
+//typeof null &non primitve datatype ka object aaata , 
+// function ka function object hotta though dikhata function hai 
+//undefined ka undefined hotta 
+//baaki dataype string number symbol ye sab aate 
+
+
+//----------------Lecture10-----------------------------------------
+// primitve->stack(call by copy), non-primitve->heap(calll by reference)
+let myname="Siddharth";
+
+let anothername=myname;
+anothername="Siddhu";
+
+console.log(myname);
+console.log(anothername);
+
+let user1={
+    name:"Siddharth",
+    id:1
+}
+let user2=user1;
+user2.id=2;
+console.log(user1);
+console.log(user2);
