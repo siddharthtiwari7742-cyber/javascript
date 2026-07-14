118cd4f9e2f81e181f452f0dd73d8e40d60f465a
siddharthtiwari7742-cyber <siddharthtiwari7742@gmail.com>
2026-07-14 09:54:13 +0000

    watched upto 5 lectures

---
+++ basics/datatypes.js
@@ -0,0 +1,7 @@
+"use strict ";
+// alert(3+3) this syntax is wrong for node js it works in browser console or in files with index.html 
+// documentation ke liye tc39.es , MDN
+let name= "Sidd" ;
+let age = 18;
+console.table([typeof(name),age,typeof(null)]);
+
---
+++ basics/variables.js
@@ -0,0 +1,4 @@
+console.log("siddharth");
+let i=10;
+i=20;
+console.log(i);
